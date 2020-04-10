a91627ce4d85599aa6ab27328ff6ac611a4ed9e0
Grace Berman <grace.g.berman@gmail.com>
2020-04-10 20:22:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,11 @@ def reverse_each_word(string)
 end
 
 
-def reverse_each_word(string)
-  array = string.split(" ")
-  new_array = []
-  array.each do |word|
-    new_array << word.reverse
-  end
-  new_array.join(" ")
-end
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   new_array = []
+#   array.each do |word|
+#     new_array << word.reverse
+#   end
+#   new_array.join(" ")
+# end
